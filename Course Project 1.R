2c25b348a8b2278212acc3df70d5e6ae41c7739d
lindsayo <lindsayo@240280.no-reply.drupal.org>
2015-09-09 22:43:23 -0600

    prelim commits for plot3.PNG

--- Course Project 1.R
+++ Course Project 1.R
@@ -10,3 +10,12 @@ hist(daysDF$Global_active_power, col="red", main="Global Active Power", xlab="Gl
 mutate(daysDF, dateTime = paste(daysDF$Date, daysDF$Time, sep=" "))
 daysDF$dateTime <- strptime(daysDF$dateTime, "%Y-%m-%d %H:%M:%S")
 plot(daysDF$dateTime,daysDF$Global_active_power, type="l", main="Global Active Power per Day", ylab="Global Active Power (kilowatts)")
+
+# Make the third line graph, plot3.PNG
+plot(daysDF$dateTime, daysDF$Sub_metering_1, 
+  type="l", col="black", 
+  xlab="", ylab="Energy sub metering", 
+  lines(daysDF$dateTime, daysDF$Sub_metering_2, type="l", col="red"),
+  lines(daysDF$dateTime, daysDF$Sub_metering_3, type="l", col="blue"),
+  legend(2000,9.5,c("foo","bar"),lty=c(1,1), lwd=c(2.5,2.5), col=c("blue","red"))
+)
