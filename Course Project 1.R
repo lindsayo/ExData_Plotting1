81d023440fdc961e719ffe1ec5188f95c4d5be9c
lindsayo <lindsayo@240280.no-reply.drupal.org>
2015-09-09 19:08:34 -0600

    course project 1, plot 1

---
+++ Course Project 1.R
@@ -0,0 +1,5 @@
+df <- read.table("../household_power_consumption.txt", header=T, sep=";")
+df$Date <- as.Date(df$Date, "%d/%m/%Y")
+daysDF <- subset(df, Date == '2007-02-01' | Date == '2007-02-02')
+daysDF$Global_active_power <- as.numeric(as.character(daysDF$Global_active_power))
+hist(daysDF$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
